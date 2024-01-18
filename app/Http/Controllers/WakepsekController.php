95e462c429a1dcefe4bf20162ebc388ed7c00fb4
Rafisyah <[m.arifin.putrasetia@gmail.com]>
2024-01-18 07:32:33 +0700

    migration setengah jadi

---
+++ app/Http/Controllers/WakepsekController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\wakepsek;
+use Illuminate\Http\Request;
+
+class WakepsekController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(wakepsek $wakepsek)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(wakepsek $wakepsek)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, wakepsek $wakepsek)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(wakepsek $wakepsek)
+    {
+        //
+    }
+}
