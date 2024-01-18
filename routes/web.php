f35c8c05ec5dba3f46af2a6dd262a251eb57435b
Rafisyah <[m.arifin.putrasetia@gmail.com]>
2024-01-18 10:03:52 +0700

    navbar

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KelasController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/kelas');
 });
+
+Route::get("/kelas", [KelasController::class, "index"]);
